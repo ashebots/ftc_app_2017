200ffb532262dc572869c643c62c661922bcbd1c
artschell <arthurlschell@gmail.com>
2016-11-17 16:57:57 -0500

    Speed/Slow toggle updated, added Mechanum Wheel TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/PreMechanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/PreMechanumTeleOp.java
@@ -1,34 +1,38 @@
 package org.firstinspires.ftc.teamcode.teleop;
+
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.*;
 
-import org.ashebots.ftcandroidlib.complexOps.*;
+import org.ashebots.ftcandroidlib.complexOps.AdvMotor;
+import org.ashebots.ftcandroidlib.complexOps.AdvOpMode;
+import org.ashebots.ftcandroidlib.complexOps.BoolEvent;
+import org.ashebots.ftcandroidlib.complexOps.Chassis;
+import org.ashebots.ftcandroidlib.complexOps.JoyEvent;
 
-@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="Drive (w/ Mechanum)", group ="TeleOp")
+@TeleOp(name="Drive (w/o Mechanum)", group ="TeleOp")
 
-public class MainTeleOp extends AdvOpMode {
+public class PreMechanumTeleOp extends AdvOpMode {
     //Define hardware
     AdvMotor lift;
-    ChassisMechanum chassis;
+    Chassis chassis;
     JoyEvent s = new JoyEvent(0.1,0.15,1.0);
     JoyEvent n = new JoyEvent(0.4,0.45,1.0);
-    JoyEvent f = new JoyEvent(1.0,1.0,1.0);
+    JoyEvent f = new JoyEvent(0.9,1.0,1.0);
 
     AdvMotor sweeper;
     AdvMotor accelerator;
-
     BoolEvent aButton = new BoolEvent();
+    BoolEvent yButton = new BoolEvent();
     BoolEvent bButton = new BoolEvent();
     BoolEvent xButton = new BoolEvent();
-    BoolEvent yButton = new BoolEvent();
 
     boolean accTog;
     boolean frtTog;
-    int speedMode;
+
+    int speedMode = 0; //0 is normal, 1 is fast, 2 is slow
     @Override
     public void init() {
         lift = mtr("Lift");
-        chassis = chassismechanum("Left", "Right", "LeftBack", "RightBack");
+        chassis = chassis("Left", "Right");
         sweeper = mtr("Sweeper");
         accelerator = mtr("Accelerator");
     }
@@ -48,34 +52,23 @@ public class MainTeleOp extends AdvOpMode {
         }
 
         //Run Motors
-        if (gamepad1.right_stick_x != 0 || gamepad1.right_stick_y != 0) { //Mechanum Joystick
-            double[] mVals = {gamepad1.right_stick_x,gamepad1.right_stick_y};
-            if (speedMode == 0) { //normal mode
-                mVals[0] *= 0.4;
-                mVals[1] *= 0.4;
-            }
-            if (speedMode == 2) { //slow mode
-                mVals[0] *= 0.1;
-                mVals[1] *= 0.1;
-            }
-            chassis.mechanumDrive(mVals[0],mVals[1]);
-        } else { //Normal Joystick
-            double[] mVals = n.calc(gamepad1.left_stick_x, gamepad1.left_stick_y);
-            if (speedMode == 1) { //fast mode
-                mVals = f.calc(gamepad1.left_stick_x, gamepad1.left_stick_y);
-            }
-            if (speedMode == 2) { //slow mode
-                mVals = s.calc(gamepad1.left_stick_x, gamepad1.left_stick_y);
-            }
-            frtTog = (frtTog ^ bButton.parse(gamepad1.b).equals("PRESSED"));
-            if (frtTog) {
-                double placeholder = mVals[0];
-                mVals[0] = -mVals[1];
-                mVals[1] = -placeholder;
-            }
-            chassis.moveMotors(mVals[0], mVals[1]);
+        double[] mVals = n.calc(gamepad1.left_stick_x,gamepad1.left_stick_y);
+        if (speedMode == 1) { //fast mode
+            mVals = f.calc(gamepad1.left_stick_x,gamepad1.left_stick_y);
         }
+        if (speedMode == 2) { //slow mode
+            mVals = s.calc(gamepad1.left_stick_x,gamepad1.left_stick_y);
+        }
+        frtTog = (frtTog ^ bButton.parse(gamepad1.b).equals("PRESSED"));
+        if (frtTog) {
+            double placeholder = mVals[0];
+            mVals[0] = -mVals[1];
+            mVals[1] = -placeholder;
+        }
+        chassis.moveMotors(mVals[0], mVals[1]);
 
+        telemetry.addData("Left", mVals[0]);
+        telemetry.addData("Right", mVals[1]);
         //Toggle accelerator
         accTog = (accTog ^ yButton.parse(gamepad1.y).equals("PRESSED"));
         if (accTog) {
