f588f9e3ecc36917d8b00db3d4fd1980ffac508f
NiknAknO <familiarush@hotmail.com>
2017-05-16 18:48:55 -0400

    Chassis stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ChassisCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ChassisCode.java
@@ -1,9 +1,16 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 /**
  * Created by jezebelquit on 5/16/17.
  */
 
-public class NewChassis {
-    public NewChassis(){}
+public class ChassisCode {
+    DcMotor motorLeft;
+    DcMotor motorRight;
+    public ChassisCode(DcMotor motorLeft, DcMotor motorRight){
+        this.motorLeft = motorLeft;
+        this.motorRight = motorRight;
+    }
 }
